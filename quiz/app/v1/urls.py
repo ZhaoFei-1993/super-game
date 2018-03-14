4de33615267f89a315a43724b70a6982909f70b0
q84256948 <1418229458@qq.com>
2018-03-15 01:24:34 +0800

    竞猜下注

--- quiz/app/v1/urls.py
+++ quiz/app/v1/urls.py
@@ -16,11 +16,9 @@ urlpatterns = [
     path('<int:quiz_id>/', views.QuizDetailView.as_view(), name="app-v1-quiz-detail"),
     # 竞猜选项
     path('rule/<int:quiz_id>/', views.RuleView.as_view(), name="app-v1-quiz-rule"),
-    # 竞猜玩法下选项
-    path('bet/<int:type>/', views.BetView.as_view(), name="app-v1-quiz-bet"),
     # 切换币总
     path('coin/', views.CoinView.as_view(), name="app-v1-quiz-coin"),
     # 竞猜下注
-    path(r"^bet/$", view=views.BetView.as_view(), name="app-quiz-qm-bet"),
+    path(r'bet/', views.BetView.as_view(), name="app-v1-quiz-bet"),
 ]
 
