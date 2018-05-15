819fc4a98c2d22b5a984ae1f6879702d91cdad40
Rex <rexlin0624@gmail.com>
2018-05-15 18:29:33 +0800

    fix url

--- users/management/commands/fix_http_to_https.py
+++ users/management/commands/fix_http_to_https.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from quiz.models import Category
+from quiz.models import Category, Quiz
 
 
 class Command(BaseCommand):
@@ -10,12 +10,20 @@ class Command(BaseCommand):
     help = "批量修改http为https"
 
     def handle(self, *args, **options):
-        category = Category.objects.all()
-        for cate in category:
-            if 'https:' in cate.icon or cate.icon == '':
-                self.stdout.write(self.style.SUCCESS('ICON修改地址跳过 ' + cate.name))
-                continue
-            icon = cate.icon.replace('http:', 'https:')
-            cate.icon = icon
-            cate.save()
-            self.stdout.write(self.style.SUCCESS('ICON修改地址成功 ' + cate.name))
+        quizs = Quiz.objects.all()
+        for quiz in quizs:
+            host_avatar = quiz.host_team_avatar.replace('http:', 'https:')
+            guest_avatar = quiz.guest_team_avatar.replace('http:', 'https:')
+            quiz.host_team_avatar = host_avatar
+            quiz.guest_team_avatar = guest_avatar
+            quiz.save()
+            self.stdout.write(self.style.SUCCESS('地址成功 ' + quiz.host_team + ' VS ' + quiz.guest_team))
+        # category = Category.objects.all()
+        # for cate in category:
+        #     if 'https:' in cate.icon or cate.icon == '':
+        #         self.stdout.write(self.style.SUCCESS('ICON修改地址跳过 ' + cate.name))
+        #         continue
+        #     icon = cate.icon.replace('http:', 'https:')
+        #     cate.icon = icon
+        #     cate.save()
+        #     self.stdout.write(self.style.SUCCESS('ICON修改地址成功 ' + cate.name))
