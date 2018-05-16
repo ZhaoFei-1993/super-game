83186f27fd2bd7f0be0148396e7ee7b107b8f111
zhijiefong <15812913276@139.com>
2018-05-16 15:15:49 +0800

    test

--- users/app/v1/views.py
+++ users/app/v1/views.py
@@ -885,6 +885,7 @@ class AssetView(ListAPIView):
             if temp_dict['coin_name']=='HAND':
                 temp_dict['eth_balance']=normalize_fraction(eth.balance)
                 temp_dict['eth_address']=eth.address
+                temp_dict['eth_coin_id']=eth.coin_id
             data.append(temp_dict)
 
         return self.response({'code': 0, 'user_name': user_info.username, 'user_avatar': user_info.avatar,
