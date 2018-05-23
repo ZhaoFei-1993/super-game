0385c2b2869992ada38a3406beba8186a2840173
Rex <rexlin0624@gmail.com>
2018-05-23 14:17:27 +0800

    机器人脚本调整

--- users/management/commands/delete_old_robot.py
+++ users/management/commands/delete_old_robot.py
@@ -13,21 +13,21 @@ class Command(BaseCommand):
 
     @transaction.atomic()
     def handle(self, *args, **options):
-        robot_users = User.objects.filter(is_robot=True, pk__lt=1364)
-        self.stdout.write(self.style.SUCCESS('获取到 ' + str(len(robot_users)) + ' 个机器人'))
+        user_coins = UserCoin.objects.all()
 
         user_ids = []
-        for robot in robot_users:
-            user_ids.append(robot.id)
+        users = User.objects.all()
+        for user in users:
+            user_ids.append(user.id)
 
-        records = Record.objects.filter(user_id__in=user_ids)
-        records.delete()
-        self.stdout.write(self.style.SUCCESS('已删除 ' + str(len(records)) + ' 条投注记录'))
+        delete_user_ids = []
+        for user_coin in user_coins:
+            if user_coin.user_id not in user_ids:
+                delete_user_ids.append(user_coin.user_id)
 
-        user_coins = UserCoin.objects.filter(user_id__in=user_ids)
-        user_coins.delete()
-        self.stdout.write(self.style.SUCCESS('已删除 ' + str(len(user_coins)) + ' 条用户货币记录'))
+        self.stdout.write(self.style.SUCCESS('获取到' + str(len(delete_user_ids)) + '条可删除用户'))
 
-        robot_users.delete()
+        UserCoin.objects.filter(user_id__in=delete_user_ids).delete()
+        Record.objects.filter(user_id__in=delete_user_ids).delete()
 
         self.stdout.write(self.style.SUCCESS('清除成功'))
