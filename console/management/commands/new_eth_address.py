bcd488e48a84cace8c8872ec19321ca8e7f170c1
Rex <rexlin0624@gmail.com>
2018-05-27 10:39:37 +0800

    调整ETH地址生成数量

--- console/management/commands/new_eth_address.py
+++ console/management/commands/new_eth_address.py
@@ -5,29 +5,15 @@ from console.models import Address
 from users.models import Coin
 from base.eth import *
 
-user_id = 43
-
 
 class Command(BaseCommand):
     help = "分配ETH地址"
+    max_address = 500
 
     def handle(self, *args, **options):
         eth_wallet = Wallet()
-        # json_data = eth_wallet.post(url='v1/account/new', data=None)
 
-        # if Address.objects.filter(user=User.objects.filter(id=user_id).first()). \
-        #         filter(coin=Coin.objects.filter(name='ETH').first()).first() is None:
-        #     address_query = Address()
-        #     address_query.coin = Coin.objects.filter(name='ETH').first()
-        #     address_query.address = json_data['data']['account']
-        #     address_query.passphrase = json_data['data']['password']
-        #     address_query.user = User.objects.filter(id=user_id).first()
-        #     address_query.save()
-        #     print('分配成功')
-        # else:
-        #     print('用户已分配地址')
-
-        for i in range(0, 500):
+        for i in range(0, self.max_address):
             json_data = eth_wallet.post(url='v1/account/new', data=None)
             if json_data['code'] != 0:
                 print(json_data['message'])
@@ -39,3 +25,5 @@ class Command(BaseCommand):
             address_query.passphrase = json_data['data']['password']
             address_query.save()
             print('分配成功 ' + str(i))
+
+        self.stdout.write(self.style.SUCCESS('成功生成 ' + str(self.max_address) + ' 条ETH地址'))
