a65f00e9d71f933808c33e1fb4b911eb6a0c7daa
Rex <rexlin0624@gmail.com>
2018-05-28 09:08:39 +0800

    调整生成ETH机制

--- console/management/commands/new_eth_address.py
+++ console/management/commands/new_eth_address.py
@@ -13,6 +13,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         eth_wallet = Wallet()
 
+        print('****************正在获取ETH地址******************')
+        self.stdout.write(self.style.SUCCESS('****************正在获取ETH地址******************'))
+
+        idx = 1
         for i in range(0, self.max_address):
             json_data = eth_wallet.post(url='v1/account/new', data=None)
             if json_data['code'] != 0:
@@ -24,6 +28,9 @@ class Command(BaseCommand):
             address_query.address = json_data['data']['account']
             address_query.passphrase = json_data['data']['password']
             address_query.save()
-            print('分配成功 ' + str(i))
 
+            idx += 1
+            print('分配成功 ' + str(idx))
+
+        print('成功生成 ' + str(self.max_address) + ' 条ETH地址')
         self.stdout.write(self.style.SUCCESS('成功生成 ' + str(self.max_address) + ' 条ETH地址'))
