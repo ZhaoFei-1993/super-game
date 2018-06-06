3884ec031353a9ae1547f51d6386032f6fcdcdff
zhijiefong <15812913276@139.com>
2018-06-06 17:48:15 +0800

    test

--- users/app/v1/views.py
+++ users/app/v1/views.py
@@ -2240,7 +2240,7 @@ class ActivityImageView(ListAPIView):
     """
 
     def get(self, request, *args, **kwargs):
-        activity_img = '/'.join([MEDIA_DOMAIN_HOST, 'ATI.jpg'])
+        activity_img = '/'.join([MEDIA_DOMAIN_HOST, 'uploads', 'ATI.jpg'])
         return self.response(
             {'code': 0, 'data': [{'img_url': activity_img, 'action': 'Activity', 'activity_name': "充值福利"}]})
 
