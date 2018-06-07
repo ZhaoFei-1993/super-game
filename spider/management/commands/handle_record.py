2b465bded7cffc6eb69813ee2976a3cb676f2ffb
q84256948 <1418229458@qq.com>
2018-06-07 17:34:57 +0800

    货币models改变

--- spider/management/commands/handle_record.py
+++ spider/management/commands/handle_record.py
@@ -2,14 +2,13 @@
 
 from django.core.management.base import BaseCommand
 from quiz.models import Record
-from django.db.models import Q
 
 
 class Command(BaseCommand):
     help = "修改记录"
 
     def handle(self, *args, **options):
-        record_list = Record.objects.filter(~Q(source=1))
+        record_list = Record.objects.all()
         i = 1
         for record in record_list:
             if record.earn_coin == record.bet:
