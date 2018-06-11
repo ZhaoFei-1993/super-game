113a83765a15aabc0105919d2b81710405ba3203
q84256948 <1418229458@qq.com>
2018-06-11 10:28:23 +0800

    货币models改变

--- quiz/app/v1/views.py
+++ quiz/app/v1/views.py
@@ -614,5 +614,5 @@ class WorldCup(ListAPIView):
     def list(self, request, *args, **kwargs):
         results = super().list(request, *args, **kwargs)
         value = results.data.get('results')
-        time = 1528732800
+        time = 0
         return self.response({"code": 0, "data": value, 'time': time})
