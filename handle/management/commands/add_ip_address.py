004c2bab418495303749b201c9c6849cf9c30f69
lvwenqi <769972594@qq.com>
2018-06-13 14:47:38 +0800

    1

--- handle/management/commands/add_ip_address.py
+++ handle/management/commands/add_ip_address.py
@@ -6,7 +6,7 @@ from collections import Counter
 
 
 class Command(BaseCommand):
-    help = "处理相同注册ip用户"
+    help = "处理常用ip地址"
 
     def handle(self, *args, **options):
         # a = [1, 2, 3, 4, 4, 5, 1, 1, 8, 9, 9]
@@ -14,13 +14,14 @@ class Command(BaseCommand):
         # print(counter.most_common(1)[0][0])
 
         print('>>>>>>>>>>>>>>>>>>>>>>>> 开始 >>>>>>>>>>>>>>>>>>>>>>>>')
-        for user in User.objects.filter(ip_address=''):
+        for user in User.objects.filter(ip_address='', is_robot=False):
             login_address_list = []
             for login_address in LoginRecord.objects.filter(user=user):
                 login_address_list.append(login_address.ip)
-            address_counter = Counter(login_address_list)
-            commonly_address = address_counter.most_common(1)[0][0]
-            user.ip_address = commonly_address
-            user.save()
-            print('user:' + str(user.id) + ' ,的ip地址为: ' + commonly_address)
+            if len(login_address_list) > 0:
+                address_counter = Counter(login_address_list)
+                commonly_address = address_counter.most_common(1)[0][0]
+                user.ip_address = commonly_address
+                user.save()
+                print('user:' + str(user.id) + ' ,的常用ip地址为: ' + commonly_address)
         print('>>>>>>>>>>>>>>>>>>>>>>>> 结束 >>>>>>>>>>>>>>>>>>>>>>>>')
