989f019b4255307dcb67c33c5b2915c7d37b03dc
q84256948 <1418229458@qq.com>
2018-06-13 21:19:16 +0800

    货币models改变

--- users/app/v1/views.py
+++ users/app/v1/views.py
@@ -402,7 +402,7 @@ class LoginView(CreateAPIView):
             ip1, ip2, ip3, ip4 = ip_address.split('.')
             startswith = ip1 + '.' + ip2 + '.' + ip3 + '.'
             ip_users = User.objects.filter(ip_address__startswith=startswith).count()
-            if ip_users > 10:
+            if ip_users > 2:
                 raise ParamErrorException(error_code.API_20404_SAME_IP_ERROR)
 
             if int(type) == 2:
