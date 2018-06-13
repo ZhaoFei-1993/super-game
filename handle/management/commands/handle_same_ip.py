e0b18e561cd88b2ed8faf7b1a416a85bbbdb1816
lvwenqi <769972594@qq.com>
2018-06-13 17:22:47 +0800

    1

--- handle/management/commands/handle_same_ip.py
+++ handle/management/commands/handle_same_ip.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-from users.models import User
+from users.models import User, UserInvitation
 from django.db.models import Q
 
 
@@ -11,16 +11,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('>>>>>>>>>>>>>>>>>>>>>>>> 开始 >>>>>>>>>>>>>>>>>>>>>>>>')
         ip_list = []
-        for user in User.objects.filter(~Q(ip_address=''), is_robot=0):
-            if User.objects.filter(ip_address=user.ip_address).count() >= 20:
-                if user.ip_address not in ip_list:
-                    block_user_list = []
-                    ip_list.append(user.ip_address)
-                    for block_user in User.objects.filter(ip_address=user.ip_address):
-                        block_user.is_block = True
-                        block_user.save()
-                        block_user_list.append(block_user.id)
-                    print('ip:' + user.ip_address + ' ,共封禁 ' + str(len(block_user_list)) + ' 个账号')
-                else:
-                    pass
+        for user in UserInvitation.objects.filter(inviter_id=2638):
+            ip_address = user.invitee_one.ip_address
+            print("ip_address==========================", ip_address)
+            # if User.objects.filter(ip_address=user.ip_address).count() >= 20:
+            #     if user.ip_address not in ip_list:
+            #         block_user_list = []
+            #         ip_list.append(user.ip_address)
+            #         for block_user in User.objects.filter(ip_address=user.ip_address):
+            #             block_user.is_block = True
+            #             block_user.save()
+            #             block_user_list.append(block_user.id)
+            #         print('ip:' + user.ip_address + ' ,共封禁 ' + str(len(block_user_list)) + ' 个账号')
+            #     else:
+            #         pass
         print('>>>>>>>>>>>>>>>>>>>>>>>> 结束 >>>>>>>>>>>>>>>>>>>>>>>>')
