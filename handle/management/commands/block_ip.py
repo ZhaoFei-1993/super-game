658e996f8a23c5e68de00142f1daef50ecc61f4d
q84256948 <1418229458@qq.com>
2018-06-13 20:55:57 +0800

    货币models改变

--- handle/management/commands/block_ip.py
+++ handle/management/commands/block_ip.py
@@ -10,6 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('>>>>>>>>>>>>>>>>>>>>>>>> 开始 >>>>>>>>>>>>>>>>>>>>>>>>')
         ip_list = []
+        i = 1
         for user in User.objects.all():
             ip_address = str(user.ip_address)
             if ip_address != '':
@@ -20,10 +21,12 @@ class Command(BaseCommand):
                         block_user_list = []
                         ip_list.append(user.ip_address)
                         for block_user in User.objects.filter(ip_address=user.ip_address):
+                            i += 1
                             block_user.is_block = True
-                            block_user.save()
+                            # block_user.save()
                             block_user_list.append(block_user.id)
                         print('ip:' + user.ip_address + ' ,共封禁 ' + str(len(block_user_list)) + ' 个账号')
                 else:
                     pass
+        print('>>>>>>>>>>>>>>>>>>>>>>>> 狗带了', i, '个用户 >>>>>>>>>>>>>>>>>>>>>>>>')
         print('>>>>>>>>>>>>>>>>>>>>>>>> 结束 >>>>>>>>>>>>>>>>>>>>>>>>')
