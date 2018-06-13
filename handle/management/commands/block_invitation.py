388ecb73030c4878e43528464965e82918395f1f
lvwenqi <769972594@qq.com>
2018-06-13 18:56:34 +0800

    1

---
+++ handle/management/commands/block_invitation.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from users.models import IntInvitation, User
+
+
+class Command(BaseCommand):
+    help = "block invitation"
+
+    def handle(self, *args, **options):
+        for int_invitation in IntInvitation.objects.all():
+            user = User.objects.get(pk=int_invitation.invitee)
+            if user.is_block is True:
+                int_invitation.is_block = True
+                int_invitation.save()
