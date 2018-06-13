03d6147b6a9ee676303fb4491385f634e0322c25
q84256948 <1418229458@qq.com>
2018-06-13 21:03:25 +0800

    货币models改变

--- handle/management/commands/block_ip.py
+++ handle/management/commands/block_ip.py
@@ -16,14 +16,14 @@ class Command(BaseCommand):
             if ip_address != '':
                 ip1, ip2, ip3, ip4 = ip_address.split('.')
                 startswith = ip1 + '.' + ip2 + '.' + ip3 + '.'
-                if User.objects.filter(ip_address__startswith=startswith).count() >= 20:
+                if User.objects.filter(ip_address__startswith=startswith).count() >= 15:
                     if user.ip_address not in ip_list:
                         block_user_list = []
                         ip_list.append(user.ip_address)
                         for block_user in User.objects.filter(ip_address=user.ip_address):
                             i += 1
                             block_user.is_block = True
-                            # block_user.save()
+                            block_user.save()
                             block_user_list.append(block_user.id)
                         print('ip:' + user.ip_address + ' ,共封禁 ' + str(len(block_user_list)) + ' 个账号')
                 else:
