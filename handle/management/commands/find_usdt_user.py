4045da43641cdd58b149996687fc4ea135c04a53
lvwenqi <769972594@qq.com>
2018-06-14 21:00:07 +0800

    1

--- handle/management/commands/find_usdt_user.py
+++ handle/management/commands/find_usdt_user.py
@@ -3,6 +3,7 @@
 from django.core.management.base import BaseCommand
 from users.models import CoinGiveRecords, UserCoin
 from quiz.models import Record
+from utils.functions import normalize_fraction
 
 
 class Command(BaseCommand):
@@ -10,15 +11,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         quiz_list = []
-        for coin_give_records in CoinGiveRecords.objects.filter(is_recharge_lock=False):
+        for coin_give_records in CoinGiveRecords.objects.filter(is_recharge_lock=False, user__is_block=False,
+                                                                user__is_robot=False):
             user_coin = UserCoin.objects.filter(coin=coin_give_records.coin_give.coin,
                                                 user=coin_give_records.user).first()
-            if float(user_coin.balance) != float(coin_give_records.lock_coin):
+            if normalize_fraction(user_coin.balance, 2) != normalize_fraction(coin_give_records.lock_coin, 2):
                 print(coin_give_records.user.id, ',')
 
         print('-----------------------------------------------------------------------')
 
-        for coin_give_records in CoinGiveRecords.objects.filter(is_recharge_lock=False):
+        for coin_give_records in CoinGiveRecords.objects.filter(is_recharge_lock=False, user__is_block=False,
+                                                                user__is_robot=False):
             for record in Record.objects.filter(user=coin_give_records.user, roomquiz_id=6, source=str(Record.GIVE)):
                 if record.quiz_id not in quiz_list:
                     quiz_list.append(record.quiz_id)
