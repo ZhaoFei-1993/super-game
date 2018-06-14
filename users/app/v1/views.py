a18f3d7c73060a259fe4878cb53ffc6d1981a10e
q84256948 <1418229458@qq.com>
2018-06-14 15:41:19 +0800

    货币models改变

--- users/app/v1/views.py
+++ users/app/v1/views.py
@@ -359,7 +359,7 @@ class LoginView(CreateAPIView):
         获取已经下载的用户昵称和头像
         :return:
         """
-        key_name_avatar = 'key_name_avatar'
+        key_name_avatar = 'key_avatar'
 
         line_number = get_cache(key_name_avatar)
         if line_number is None:
