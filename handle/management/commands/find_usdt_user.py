05d568888baa2067d94c9fcc2920424f97d3084d
q84256948 <1418229458@qq.com>
2018-06-14 21:43:00 +0800

    货币models改变

--- handle/management/commands/find_usdt_user.py
+++ handle/management/commands/find_usdt_user.py
@@ -3,14 +3,13 @@
 from django.core.management.base import BaseCommand
 from users.models import CoinGiveRecords, UserCoin
 from quiz.models import Record
-from utils.functions import normalize_fraction
 
 
 class Command(BaseCommand):
     help = "寻找不同步usdt用户"
 
     def handle(self, *args, **options):
-        quiz_list = []
+        # quiz_list = []
         for coin_give_records in CoinGiveRecords.objects.filter(is_recharge_lock=False, user__is_block=False,
                                                                 user__is_robot=False):
             user_coin = UserCoin.objects.get(coin=coin_give_records.coin_give.coin, user=coin_give_records.user)
@@ -20,12 +19,12 @@ class Command(BaseCommand):
                       coin_give_records.lock_coin)
                 print(coin_give_records.user.id, ',')
 
-        print('-----------------------------------------------------------------------')
-
-        for coin_give_records in CoinGiveRecords.objects.filter(is_recharge_lock=False, user__is_block=False,
-                                                                user__is_robot=False):
-            for record in Record.objects.filter(user=coin_give_records.user, roomquiz_id=6, source=str(Record.GIVE)):
-                if record.quiz_id not in quiz_list:
-                    quiz_list.append(record.quiz_id)
-            if coin_give_records.lock_coin >= 60 and len(quiz_list) >= 6:
-                print(coin_give_records.user.id, ',')
+        # print('-----------------------------------------------------------------------')
+        #
+        # for coin_give_records in CoinGiveRecords.objects.filter(is_recharge_lock=False, user__is_block=False,
+        #                                                         user__is_robot=False):
+        #     for record in Record.objects.filter(user=coin_give_records.user, roomquiz_id=6, source=str(Record.GIVE)):
+        #         if record.quiz_id not in quiz_list:
+        #             quiz_list.append(record.quiz_id)
+        #     if coin_give_records.lock_coin >= 60 and len(quiz_list) >= 6:
+        #         print(coin_give_records.user.id, ',')
