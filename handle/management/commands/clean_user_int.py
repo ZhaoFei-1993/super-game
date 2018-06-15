cbb0aac064c3a7750ace4a2f90394d1657f22848
q84256948 <1418229458@qq.com>
2018-06-15 14:30:13 +0800

    货币models改变

--- handle/management/commands/clean_user_int.py
+++ handle/management/commands/clean_user_int.py
@@ -2,14 +2,20 @@
 
 from django.core.management.base import BaseCommand
 from users.models import User, UserCoin, Coin
+from quiz.models import Record
 
 
 class Command(BaseCommand):
     help = "被封禁用户int清零"
 
     def handle(self, *args, **options):
-        int_coin = Coin.objects.get(name='INT')
-        for block_user in User.objects.filter(is_block=True, is_robot=False):
-            block_user_int_coin = UserCoin.objects.get(user=block_user, coin=int_coin)
-            block_user_int_coin.balance = 0
-            block_user_int_coin.save()
+        # int_coin = Coin.objects.get(name='INT')
+        # for block_user in User.objects.filter(is_block=True, is_robot=False):
+        #     block_user_int_coin = UserCoin.objects.get(user=block_user, coin=int_coin)
+        #     block_user_int_coin.balance = 0
+        #     block_user_int_coin.save()
+        records = Record.objects.filter(source=Record.GIVE, user_id=5089, roomquiz_id=6).values(
+            'quiz_id').distinct().count()
+        print("records=================================", records)
+        if records < 6:
+            print("records===============================================")
