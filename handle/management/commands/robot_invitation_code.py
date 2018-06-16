fa96e0083e0b4f9c2672fa6679df3b910657762d
q84256948 <1418229458@qq.com>
2018-06-16 22:20:47 +0800

    货币models改变

--- handle/management/commands/robot_invitation_code.py
+++ handle/management/commands/robot_invitation_code.py
@@ -5,7 +5,7 @@ from users.models import User, LoginRecord
 from utils.functions import random_invitation_code
 from django.db.models import Q
 from api.settings import BASE_DIR
-import os
+import datetime
 
 
 class Command(BaseCommand):
@@ -14,7 +14,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         i = 1
         for user in User.objects.filter(is_block=1):
-            user_loging = LoginRecord.objects.filter(user_id=user.id).count()
-            if user_loging < 0:
+            create_at = user.created_at
+            log_at = LoginRecord.objects.filter(user=user).order_by('login_time').first().login_time
+            if log_at - create_at > datetime.timedelta(minutes=20):
                 i += 1
         print("i==================================", i)
