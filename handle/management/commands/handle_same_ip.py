e17b95aedef5e2741b435f7f8f17d14e6ec9dc92
q84256948 <1418229458@qq.com>
2018-06-16 20:54:34 +0800

    货币models改变

--- handle/management/commands/handle_same_ip.py
+++ handle/management/commands/handle_same_ip.py
@@ -11,10 +11,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print('>>>>>>>>>>>>>>>>>>>>>>>> 开始 >>>>>>>>>>>>>>>>>>>>>>>>')
         ip_list = []
-        # for user in IntInvitation.objects.filter(inviter_id=2638):
-        for user_info in User.objects.all():
-            # user_id = user.invitee
-            # user_info = User.objects.get(id=user_id)
+        for user in IntInvitation.objects.filter(inviter_id=2638):
+            user_id = user.invitee
+            user_info = User.objects.get(id=user_id)
             ip_address = str(user_info.ip_address)
             if ip_address != '':
                 ip1, ip2, ip3, ip4 = ip_address.split('.')
@@ -25,7 +24,7 @@ class Command(BaseCommand):
                         ip_list.append(user_info.ip_address)
                         for block_user in User.objects.filter(ip_address=user_info.ip_address):
                             block_user.is_block = True
-                            # block_user.save()
+                            block_user.save()
                             block_user_list.append(block_user.id)
                         print('ip:' + user_info.ip_address + ' ,共封禁 ' + str(len(block_user_list)) + ' 个账号')
                 else:
