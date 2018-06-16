fb8857bcdf44cb045c43a36719e915c037331051
q84256948 <1418229458@qq.com>
2018-06-16 22:16:08 +0800

    货币models改变

--- handle/management/commands/robot_invitation_code.py
+++ handle/management/commands/robot_invitation_code.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.core.management.base import BaseCommand
-from users.models import User, UserInvitation
+from users.models import User, LoginRecord
 from utils.functions import random_invitation_code
 from django.db.models import Q
 from api.settings import BASE_DIR
@@ -12,18 +12,9 @@ class Command(BaseCommand):
     help = "给机器人分配邀请码"
 
     def handle(self, *args, **options):
-        os.chdir(BASE_DIR + '/cache')
-        with open('invitation_code.txt', 'w+') as f:
-            pass
-        robot_list = User.objects.filter(is_robot=True, invitation_code='')
-        for robot in robot_list:
-            robot.invitation_code = random_invitation_code()
-            robot.save()
-
-        for robot in User.objects.filter(is_robot=True):
-            invitee_number = UserInvitation.objects.filter(~Q(invitee_one=0), inviter=robot.id, is_effective=1).count()
-            if int(invitee_number) < 5:
-                os.chdir(BASE_DIR + '/cache')
-                with open('invitation_code.txt', 'a+') as f:
-                    f.write(robot.invitation_code + "=======================" + str(invitee_number))
-                    f.write('\n')
+        i = 1
+        for user in User.objects.filter(is_block=1):
+            user_loging = LoginRecord.objects.filter(user_id=user.id).count()
+            if user_loging > 0:
+                i += 1
+        print("i==================================", i)
