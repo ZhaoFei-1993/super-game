1933287f787a5d904f6fc4abd3567dc40bbb7394
Rex <rexlin0624@gmail.com>
2018-06-17 22:57:59 +0800

    skk

--- console/management/commands/fre_eth_moniter.py
+++ console/management/commands/fre_eth_moniter.py
@@ -54,7 +54,7 @@ class Command(BaseCommand, BaseView):
         for uea in aaaa:
             user_ids.append(str(uea[0]))
 
-        sql1 = 'SELECT * FROM users_usercoin WHERE user_id IN(' + ','.join(user_ids) + ') WHERE coin_id = 2'
+        sql1 = 'SELECT * FROM users_usercoin WHERE user_id IN(' + ','.join(user_ids) + ') AND coin_id = 2'
         user_eth_address = self.get_all_by_sql(sql1)
 
         eth_address_length = len(user_eth_address)
