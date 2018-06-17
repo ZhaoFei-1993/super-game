5d378df3f51c8e0b61334d5738802d31dda975f4
q84256948 <1418229458@qq.com>
2018-06-17 10:23:46 +0800

    货币models改变

--- api/settings.py
+++ api/settings.py
@@ -85,7 +85,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'base.middleware.RequestExceptionHandler',
-    'django.middleware.http.SetRemoteAddrFromForwardedFor'
 ]
 
 ROOT_URLCONF = 'api.urls'
