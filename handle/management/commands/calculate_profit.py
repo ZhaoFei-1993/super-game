8e980a6cfb490fac7073ceb6d44fe46d92dcc9a9
lvwenqi <769972594@qq.com>
2018-06-18 10:05:34 +0800

    1

--- handle/management/commands/calculate_profit.py
+++ handle/management/commands/calculate_profit.py
@@ -3,8 +3,9 @@
 from django.core.management.base import BaseCommand
 import datetime
 from django.db.models import Q
-from quiz.models import Record, ClubProfitAbroad, Quiz
+from quiz.models import Record, ClubProfitAbroad, Quiz, CashBackLog
 from chat.models import Club
+from users.models import CoinPrice
 from utils.functions import normalize_fraction
 
 
@@ -29,10 +30,12 @@ class Command(BaseCommand):
                 quiz_list.append(quiz.id)
             print(quiz_list)
             for club in Club.objects.filter(~Q(room_title='HAND俱乐部')):
+                coin_price = CoinPrice.objects.get(coin_name=club.room_title[:-3])
                 robot_platform_sum = 0
                 platform_sum = 0
                 profit_robot = 0
                 profit_user = 0
+                cash_back_sum = 0
                 records = Record.objects.filter(roomquiz_id=club.id, is_distribution=True, quiz__id__in=quiz_list)
                 if len(records) > 0:
                     for record in records:
@@ -61,18 +64,40 @@ class Command(BaseCommand):
 
                     profit_total = float(profit_user) + float(profit_robot)
 
+                    for cash_back_log in CashBackLog.objects.filter(roomquiz_id=club.id, quiz__id__in=quiz_list):
+                        cash_back_sum = float(cash_back_sum) + float(cash_back_log.cash_back_sum)
+
                     profit_table = ClubProfitAbroad()
                     profit_table.roomquiz_id = club.pk
+
                     profit_table.robot_platform_sum = normalize_fraction(robot_platform_sum, 3)
+                    profit_table.robot_platform_rmb = normalize_fraction(float(robot_platform_sum) * float(coin_price.price), 8)
+
                     profit_table.platform_sum = normalize_fraction(platform_sum, 3)
+                    profit_table.platform_rmb = normalize_fraction(float(platform_sum) * float(coin_price.price), 8)
+
                     profit_table.profit = normalize_fraction(profit_user, 3)
+                    profit_table.profit_rmb = normalize_fraction(float(profit_user) * float(coin_price.price), 8)
+
                     profit_table.profit_total = normalize_fraction(profit_total, 3)
+                    profit_table.profit_total_rmb = normalize_fraction(float(profit_total) * float(coin_price.price), 8)
+
+                    profit_table.cash_back_sum = normalize_fraction(cash_back_sum, 2)
                     profit_table.save()
 
                     print('=======================================>', club.room_title)
                     print('robot_platform_sum = ', normalize_fraction(robot_platform_sum, 3))
+                    print('robot_platform_rmb = ', normalize_fraction(float(robot_platform_sum) * float(coin_price.price), 8))
+
                     print('platform_sum = ', normalize_fraction(platform_sum, 3))
+                    print('platform_rmb = ', normalize_fraction(float(platform_sum) * float(coin_price.price), 8))
+
                     print('profit_robot = ', normalize_fraction(profit_robot, 3))
                     print('profit_user = ', normalize_fraction(profit_user, 3))
+                    print('profit_rmb = ', normalize_fraction(float(profit_user) * float(coin_price.price), 8))
+
                     print('profit_total = ', normalize_fraction(profit_total, 3))
+                    print('profit_total_rmb', normalize_fraction(float(profit_total) * float(coin_price.price), 8))
+
+                    print('cash_back_sum = ', normalize_fraction(cash_back_sum, 2))
                     print('----------------------------------------')
