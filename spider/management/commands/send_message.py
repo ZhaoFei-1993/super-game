1222545d95bc8dc7c1dc04a00b5509605f5bd1da
q84256948 <1418229458@qq.com>
2018-06-22 22:29:45 +0800

    货币models改变

--- spider/management/commands/send_message.py
+++ spider/management/commands/send_message.py
@@ -8,12 +8,14 @@ class Command(BaseCommand):
     help = "发送消息"
 
     def handle(self, *args, **options):
-        u_mes = Message()
-        u_mes.type = 1
-        u_mes.title = '每日签到赠送活动更换通知'
-        u_mes.content = '亲爱的用户：\n' \
-                        '超级游戏第一波签到赠送GSG已经圆满结束啦！第二波蓄势待发！从6月24日起，每日签到由之前的送GSG更换为签到送HAND。\n\n' \
-                        '给您带来不便敬请谅解，如您有任何疑问，欢迎随时联系在线客服微信 gsgone。感谢您对我们的支持与信任，祝您愉快!\n\n' \
-                        '                                                GSG超级游戏团队\n' \
-                        '                                                2018年6月22日\n'
-        u_mes.save()
+        message = Message.objects.all().order_by('-id')
+        print("message======================", message[0].content)
+        # u_mes = Message()
+        # u_mes.type = 1
+        # u_mes.title = '每日签到赠送活动更换通知'
+        # u_mes.content = '亲爱的用户：\n' \
+        #                 '超级游戏第一波签到赠送GSG已经圆满结束啦！第二波蓄势待发！从6月24日起，每日签到由之前的送GSG更换为签到送HAND。\n\n' \
+        #                 '给您带来不便敬请谅解，如您有任何疑问，欢迎随时联系在线客服微信 gsgone。感谢您对我们的支持与信任，祝您愉快!\n\n' \
+        #                 '                                                GSG超级游戏团队\n' \
+        #                 '                                                2018年6月22日\n'
+        # u_mes.save()
