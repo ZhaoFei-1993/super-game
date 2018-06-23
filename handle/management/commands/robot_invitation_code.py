46b9964a9b27b5a8964a274bb1bb14af2412e045
q84256948 <1418229458@qq.com>
2018-06-24 00:21:08 +0800

    货币models改变

--- handle/management/commands/robot_invitation_code.py
+++ handle/management/commands/robot_invitation_code.py
@@ -11,22 +11,22 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
       a=DailySettings.objects.get(id=8)
       a.rewards = 200
-      s.save()
-      a=DailySettings.objects.get(id=9)
-      a.rewards = 300
-      s.save()
-      a=DailySettings.objects.get(id=10)
-      a.rewards = 400
-      s.save()
-      a=DailySettings.objects.get(id=11)
-      a.rewards = 900
-      s.save()
-      a=DailySettings.objects.get(id=12)
-      a.rewards = 1000
-      s.save()
-      a=DailySettings.objects.get(id=13)
-      a.rewards = 1100
-      s.save()
-      a=DailySettings.objects.get(id=14)
-      a.rewards = 2000
-      s.save()
+      a.save()
+      b=DailySettings.objects.get(id=9)
+      b.rewards = 300
+      b.save()
+      c=DailySettings.objects.get(id=10)
+      c.rewards = 400
+      c.save()
+      d=DailySettings.objects.get(id=11)
+      d.rewards = 900
+      d.save()
+      e=DailySettings.objects.get(id=12)
+      e.rewards = 1000
+      e.save()
+      f=DailySettings.objects.get(id=13)
+      f.rewards = 1100
+      f.save()
+      i=DailySettings.objects.get(id=14)
+      i.rewards = 2000
+      i.save()
