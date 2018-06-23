1a50a294cc184ab1d29a803156c8dc612a49a43f
q84256948 <1418229458@qq.com>
2018-06-24 00:19:57 +0800

    货币models改变

--- handle/management/commands/robot_invitation_code.py
+++ handle/management/commands/robot_invitation_code.py
@@ -9,8 +9,24 @@ class Command(BaseCommand):
     help = "给机器人分配邀请码"
 
     def handle(self, *args, **options):
-      a=DailySettings.objects.all()
-      coin=Coin.objects.get(id=4)
-      for s in a:
-          s.coin=coin
-          s.save()
+      a=DailySettings.objects.get(id=8)
+      a.rewards = 200
+      s.save()
+      a=DailySettings.objects.get(id=9)
+      a.rewards = 300
+      s.save()
+      a=DailySettings.objects.get(id=10)
+      a.rewards = 400
+      s.save()
+      a=DailySettings.objects.get(id=11)
+      a.rewards = 900
+      s.save()
+      a=DailySettings.objects.get(id=12)
+      a.rewards = 1000
+      s.save()
+      a=DailySettings.objects.get(id=13)
+      a.rewards = 1100
+      s.save()
+      a=DailySettings.objects.get(id=14)
+      a.rewards = 2000
+      s.save()
