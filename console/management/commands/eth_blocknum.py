8ede07dcdd303afbf8e40aeeeeb9f4ebc2569b77
Yang <guanhui07@gmail.com>
2018-07-07 14:58:12 +0800

    eth_blocknum queue modify

--- console/management/commands/eth_blocknum.py
+++ console/management/commands/eth_blocknum.py
@@ -3,31 +3,33 @@ from django.core.management.base import BaseCommand, CommandError
 from base.eth import *
 from time import time
 from base.app import BaseView
-from utils.cache import get_cache, set_cache, redis_lpush
-
+from utils.cache import get_cache, set_cache
+from console.consumers import  consumer_ethblock
+from redis import Redis
+from rq import Queue
 
 class Command(BaseCommand, BaseView):
     help = "获取ETH_blocknum"
     cacheKey = 'pre_eth_block_num'
-    listKey = 'pre_eth_block_list'
 
     def add_arguments(self, parser):
         parser.add_argument('blocknum', type=str)
 
     def handle(self, *args, **options):
-        param_blocknum = int(options['blocknum'])  # 从参数中传进
+        param_blocknum = int(options['blocknum'])
+        start_time = time()
         if param_blocknum != 0:
             set_cache(self.cacheKey, param_blocknum, 86400)
 
         eth_wallet = Wallet()
-        start_time = time()
         obj = eth_wallet.get(url='/api/v1/account/gethightblock')
-        content = obj['data']['blocknum']
+        content = int(obj['data']['blocknum'])
+
         print('获取到block:', content)
         if not content:
             raise CommandError('获取blocknum失败')
 
-        cache_blocknum = get_cache(self.cacheKey)
+        cache_blocknum = int(get_cache(self.cacheKey))
 
         if cache_blocknum is None:
             raise CommandError('首次运行设置blocknum')
@@ -37,9 +39,13 @@ class Command(BaseCommand, BaseView):
         if cache_blocknum < content:
             set_cache(self.cacheKey, content, 86400)
             print('content:', content)
-            for i in range(int(cache_blocknum), int(content)):
-                redis_lpush(self.listKey, i)
+            for block_number in range(cache_blocknum, content):
+                # 消息队列
+                redis_conn = Redis()
+                q = Queue(connection=redis_conn)
+                q.enqueue(consumer_ethblock, block_number)
 
         stop_time = time()
+
         cost_time = str(round(stop_time - start_time)) + '秒'
         self.stdout.write(self.style.SUCCESS('执行完成。耗时：' + cost_time))
