ea9b9712b3a2f478c26a4f7b6ea8a09090a1dd1a
Rex <rexlin0624@gmail.com>
2018-07-07 16:26:02 +0800

    fix

--- console/consumers.py
+++ console/consumers.py
@@ -2,44 +2,43 @@ from base.eth import *
 from time import time
 import time as format_time
 from decimal import Decimal
-from users.models import UserRecharge, Coin, UserCoin, CoinDetail, User
+from users.models import UserRecharge, Coin, UserCoin, CoinDetail
 
 
 def consumer_ethblock(block_num):
     """
     消费block_num队列
     """
-    help = "消费ETH_blocknum获取取交易数据"
     eth_wallet = Wallet()
     start_time = time()
 
-    print(block_num)
+    print('block_num = ', block_num)
     if block_num is 'None':
         print('队列暂时无数据')
         return True
 
     # 根据block_num 获取交易数据
     json_obj = eth_wallet.get(url='/api/v1/chain/blocknum/' + str(block_num))
-    print(json_obj)
+    # print(json_obj)
     if json_obj['code'] != 0:
         print('根据block_num获取数据失败')
         return True
 
-    list = json_obj['data']['data']
-    print(list)
+    items = json_obj['data']['data']
+    print('items = ', items)
 
     tmp_num = 0
-    for i, val in enumerate(list):
+    for i, val in enumerate(items):
         tmp_dict = val
         tmp_dict['t_time'] = json_obj['data']['t_time']
         tmp_dict['type'] = val['type'].upper()
 
         # 根据交易信息处理db数据
-        ret = dealDbData(tmp_dict)
+        ret = deal_db_data(tmp_dict)
         if ret is True:
             tmp_num += 1
 
-    print('blocknum:' + str(block_num) +'获取到' + str(tmp_num) + '条交易信息')
+    print('blocknum:' + str(block_num) + '获取到' + str(tmp_num) + '条交易信息')
 
     stop_time = time()
     cost_time = str(round(stop_time - start_time)) + '秒'
@@ -47,7 +46,7 @@ def consumer_ethblock(block_num):
     return True
 
 
-def dealDbData(dict):
+def deal_db_data(dict):
     # dict['type'] = 'INT'
     info = Coin.objects.filter(name=dict['type'])
     if not info:
@@ -58,7 +57,7 @@ def dealDbData(dict):
     txid = dict['hash']
     addr = dict['to']
 
-    #addr = '0xbc188Cc44428b38e115a2C693C9D0a4fD0BDCc71'
+    # addr = '0xbc188Cc44428b38e115a2C693C9D0a4fD0BDCc71'
     value = dict['value']
     t_time = dict['t_time']
     usercoin_info = UserCoin.objects.filter(address=addr, coin_id=coin_id)
@@ -67,7 +66,7 @@ def dealDbData(dict):
 
     user_id = usercoin_info[0].user_id
 
-    #用户充值记录
+    # 用户充值记录
     charge_info = UserRecharge.objects.filter(address=addr, txid=txid)
     if charge_info:
         print('addr___', charge_info[0].address)
@@ -85,11 +84,11 @@ def dealDbData(dict):
         recharge_obj.amount = value
         recharge_obj.confirmations = 0
         recharge_obj.trade_at = time_dt
-        recharge_obj.save()
+        # recharge_obj.save()
 
         # 刷新用户余额表
         usercoin_info[0].balance += Decimal(value)
-        usercoin_info[0].save()
+        # usercoin_info[0].save()
 
         # 用户余额变更记录
         coin_detail = CoinDetail()
@@ -98,7 +97,7 @@ def dealDbData(dict):
         coin_detail.amount = value
         coin_detail.rest = usercoin_info[0].balance
         coin_detail.sources = CoinDetail.RECHARGE
-        coin_detail.save()
+        # coin_detail.save()
         return True
 
     return False
