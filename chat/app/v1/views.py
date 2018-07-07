7f14f52619c007176e312c4e0eec8a8d2d143196
q84256948 <1418229458@qq.com>
2018-07-07 17:47:03 +0800

    Merge branch 'luo'

--- chat/app/v1/views.py
+++ chat/app/v1/views.py
@@ -21,7 +21,18 @@ class ClublistView(ListAPIView):
     serializer_class = ClubListSerialize
 
     def get_queryset(self):
-        chat_list = Club.objects.filter(is_dissolve=0).order_by('-is_recommend')
+        if 'name' in self.request.GET:
+            name = self.request.GET.get('name')
+
+            if self.request.GET.get('language') == 'en':
+                chat_list = Club.objects.filter(
+                    Q(room_title_en__icontains=name) | Q(room_number__istartswith=name)).order_by('-is_recommend')
+            else:
+                chat_list = Club.objects.filter(
+                    Q(room_title__icontains=name) | Q(room_number__istartswith=name)).order_by(
+                    '-is_recommend')
+        else:
+            chat_list = Club.objects.filter(is_dissolve=0).order_by('-is_recommend')
         return chat_list
 
     def list(self, request, *args, **kwargs):
@@ -65,50 +76,3 @@ class ClublistView(ListAPIView):
         return self.response({"code": 0, "banner": banner, "data": data})
 
 
-class ClubSearchView(ListAPIView):
-    """
-    俱乐部搜索
-    """
-
-    permission_classes = (LoginRequired,)
-    serializer_class = ClubListSerialize
-
-    def get_queryset(self):
-        name = self.request.GET.get('name')
-
-        if self.request.GET.get('language') == 'en':
-            chat_list = Club.objects.filter(
-                Q(room_title_en__icontains=name) | Q(room_number__istartswith=name)).order_by('-is_recommend')
-        else:
-            chat_list = Club.objects.filter(Q(room_title__icontains=name) | Q(room_number__istartswith=name)).order_by(
-                '-is_recommend')
-        return chat_list
-
-    def list(self, request, *args, **kwargs):
-        results = super().list(request, *args, **kwargs)
-        items = results.data.get('results')
-        user = request.user
-        if user.is_block == 1:
-            raise ParamErrorException(error_code.API_70203_PROHIBIT_LOGIN)
-        data = []
-        for item in items:
-            user_number = int(int(item['user_number']) * 0.3)
-            room_title = language_switch(self.request.GET.get('language'), 'room_title')
-            autograph = language_switch(self.request.GET.get('language'), 'autograph')
-            room_title = item[room_title]
-            autograph = item[autograph]
-            data.append(
-                {
-                    "club_id": item['id'],
-                    "room_title": room_title,
-                    "autograph": autograph,
-                    "user_number": user_number,
-                    "room_number": item['room_number'],
-                    "coin_name": item['coin_name'],
-                    "coin_key": item['coin_key'],
-                    "icon": item['icon'],
-                    "coin_icon": item['coin_icon'],
-                    "is_recommend": item['is_recommend']
-                }
-            )
-        return self.response({"code": 0, "data": data})
