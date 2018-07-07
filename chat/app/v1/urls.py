a9351dda2892418ba1ead79ba477c179a89cc1d8
q84256948 <1418229458@qq.com>
2018-07-07 17:25:21 +0800

    Merge branch 'luo'

--- chat/app/v1/urls.py
+++ chat/app/v1/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     #  俱乐部列表
     path('clublist/', views.ClublistView.as_view(), name="chat-club-list"),
+    #  俱乐部模糊搜索
+    path('club/search/', views.ClubSearchView.as_view(), name="chat-club-search"),
 ]
--- chat/app/v1/views.py
+++ chat/app/v1/views.py
@@ -10,6 +10,7 @@ from utils.functions import language_switch
 from base import code as error_code
 from datetime import datetime
 from base.exceptions import ParamErrorException
+from django.db.models import Q
 
 
 class ClublistView(ListAPIView):
@@ -20,7 +21,7 @@ class ClublistView(ListAPIView):
     serializer_class = ClubListSerialize
 
     def get_queryset(self):
-        chat_list = Club.objects.filter(is_dissolve=0).order_by('-is_recommend').order_by("-is_recommend")
+        chat_list = Club.objects.filter(is_dissolve=0).order_by('-is_recommend')
         return chat_list
 
     def list(self, request, *args, **kwargs):
@@ -37,11 +38,10 @@ class ClublistView(ListAPIView):
         usdt_ban = '/'.join(
             [MEDIA_DOMAIN_HOST, language_switch(self.request.GET.get('language'), "USDT") + ".jpg?t=%s" % date_now])
         int_act_ban = '/'.join(
-            # [MEDIA_DOMAIN_HOST, language_switch(self.request.GET.get('language'), "INT_ACT") + ".jpg?t=%s" % date_now])
             [MEDIA_DOMAIN_HOST, "INT_ACT.jpg?t=%s" % date_now])
-        banner = ([] if language=='en' else [{"img_url": int_ban, "action": 'Invite_New'}]) \
-                  + [{"img_url": usdt_ban, "action": 'USDT_ACTIVE'}] \
-                  + ([] if language=='en' else [{"img_url": int_act_ban, "action": 'INT_COIN_ACTIVITY'}]) # 活动轮播图
+        banner = ([] if language == 'en' else [{"img_url": int_ban, "action": 'Invite_New'}]) \
+                 + [{"img_url": usdt_ban, "action": 'USDT_ACTIVE'}] \
+                 + ([] if language == 'en' else [{"img_url": int_act_ban, "action": 'INT_COIN_ACTIVITY'}])  # 活动轮播图
         for item in items:
             user_number = int(int(item['user_number']) * 0.3)
             room_title = language_switch(self.request.GET.get('language'), 'room_title')
@@ -63,3 +63,52 @@ class ClublistView(ListAPIView):
                 }
             )
         return self.response({"code": 0, "banner": banner, "data": data})
+
+
+class ClubSearchView(ListAPIView):
+    """
+    俱乐部搜索
+    """
+
+    permission_classes = (LoginRequired,)
+    serializer_class = ClubListSerialize
+
+    def get_queryset(self):
+        name = self.request.GET.get('name')
+
+        if self.request.GET.get('language') == 'en':
+            chat_list = Club.objects.filter(
+                Q(room_title_en__icontains=name) | Q(room_number__istartswith=name)).order_by('-is_recommend')
+        else:
+            chat_list = Club.objects.filter(Q(room_title__icontains=name) | Q(room_number__istartswith=name)).order_by(
+                '-is_recommend')
+        return chat_list
+
+    def list(self, request, *args, **kwargs):
+        results = super().list(request, *args, **kwargs)
+        items = results.data.get('results')
+        user = request.user
+        if user.is_block == 1:
+            raise ParamErrorException(error_code.API_70203_PROHIBIT_LOGIN)
+        data = []
+        for item in items:
+            user_number = int(int(item['user_number']) * 0.3)
+            room_title = language_switch(self.request.GET.get('language'), 'room_title')
+            autograph = language_switch(self.request.GET.get('language'), 'autograph')
+            room_title = item[room_title]
+            autograph = item[autograph]
+            data.append(
+                {
+                    "club_id": item['id'],
+                    "room_title": room_title,
+                    "autograph": autograph,
+                    "user_number": user_number,
+                    "room_number": item['room_number'],
+                    "coin_name": item['coin_name'],
+                    "coin_key": item['coin_key'],
+                    "icon": item['icon'],
+                    "coin_icon": item['coin_icon'],
+                    "is_recommend": item['is_recommend']
+                }
+            )
+        return self.response({"code": 0, "data": data})
