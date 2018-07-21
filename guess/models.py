1fc60d65e6337217bcc1288f05544d7d16122a8c
q84256948 <1418229458@qq.com>
2018-07-21 11:07:59 +0800

    Merge branch 'luo'

--- guess/models.py
+++ guess/models.py
@@ -32,10 +32,8 @@ class Stock(models.Model):
         (HANGSENG, "恒⽣指数"),
         (DOWJONES, "道琼斯")
     )
-    stock_id = models.CharField(verbose_name="证券名称", choices=STOCK, max_length=1, default=SSE)
-    stock_id_en = models.CharField(verbose_name="证券名称(英语)", choices=STOCK_EN, max_length=1, default=SSE)
-    rotary_header_time = models.DateTimeField(verbose_name="封盘时间")
-    lottery_time = models.DateTimeField(verbose_name="开奖时间")
+    name = models.CharField(verbose_name="证券名称", choices=STOCK, max_length=1, default=SSE)
+    name_en = models.CharField(verbose_name="证券名称(英语)", choices=STOCK_EN, max_length=1, default=SSE)
     order = models.IntegerField(verbose_name="排序", default=0)
     is_delete = models.BooleanField(verbose_name="是否删除", default=False)
     created_at = models.DateTimeField(verbose_name="创建时间(年月日)", auto_now_add=True)
@@ -44,6 +42,27 @@ class Stock(models.Model):
     class Meta:
         ordering = ['-id']
         verbose_name = verbose_name_plural = "股票配置表"
+
+@reversion.register()
+class Periods(models.Model):
+    periods = models.IntegerField(verbose_name='期数，递增，不中断')
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    start_value = models.DecimalField(verbose_name='开盘指数', max_digits=10, decimal_places=2, null=True)
+    lottery_value = models.DecimalField(verbose_name='开奖指数', max_digits=10, decimal_places=2, null=True)
+    lottery_time = models.DateTimeField(verbose_name='开奖时间', auto_now_add=True)
+    start_time = models.DateTimeField(verbose_name='开始下注时间', auto_now_add=True)
+    rotary_header_time = models.DateTimeField(verbose_name='封盘时间', auto_now_add=True)
+    is_seal = models.BooleanField(verbose_name="是否封盘", default=False)
+    up_and_down = models.CharField(verbose_name='涨跌正确选项', max_length=20, default='')
+    size = models.CharField(verbose_name='大小正确选项', max_length=20, default='')
+    points = models.CharField(verbose_name='点数正确选项', max_length=20, default='')
+    pair = models.CharField(verbose_name='对子正确选项', max_length=20, default='')
+    created_at = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_at = models.DateTimeField(verbose_name='更新时间', auto_now_add=True)
+
+    class Meta:
+        ordering = ['-id']
+        verbose_name = verbose_name_plural = "期数，开奖后生成下一期记录"
 
 
 @reversion.register()
@@ -85,32 +104,8 @@ class Play(models.Model):
 
 @reversion.register()
 class BetLimit(models.Model):
-    SIZE = 1
-    POINTS = 2
-    PAIR = 3
-    UP_AND_DOWN = 0
-
-    PLAY = (
-        (SIZE, "⼤⼩"),
-        (POINTS, "点数"),
-        (PAIR, "对⼦"),
-        (UP_AND_DOWN, "总和")
-    )
-
-    SIZE = 1
-    POINTS = 2
-    PAIR = 3
-    UP_AND_DOWN = 0
-
-    PLAY_EN = (
-        (SIZE, "Size"),
-        (POINTS, "Points"),
-        (PAIR, "Pair"),
-        (UP_AND_DOWN, "up_and_down")
-    )
-    club = models.ForeignKey(Club, on_delete=models.DO_NOTHING)
-    play_name = models.CharField(verbose_name="玩法", choices=PLAY, max_length=1, default=SIZE)
-    play_name_en = models.CharField(verbose_name="玩法(英语)", choices=PLAY_EN, max_length=1, default=SIZE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    play = models.ForeignKey(Play, on_delete=models.CASCADE)
     bets_one = models.CharField(verbose_name='下注值1', max_length=255, default=0.01)
     bets_two = models.CharField(verbose_name='下注值2', max_length=255, default=0.02)
     bets_three = models.CharField(verbose_name='下注值3', max_length=255, default=0.03)
@@ -124,38 +119,16 @@ class BetLimit(models.Model):
 class Options(models.Model):
     play = models.ForeignKey(Play, on_delete=models.CASCADE)
     title = models.CharField(verbose_name='选项标题', max_length=255)
-    sub_title = models.CharField(verbose_name='选项子标题', max_length=255)
     title_en = models.CharField(verbose_name='选项标题(英文)', max_length=255)
-    sub_title_en = models.CharField(verbose_name='选项子标题(英文)', max_length=255)
     odds = models.DecimalField(verbose_name='赔率', max_digits=10, decimal_places=2, default=0.00)
+    sub_title = models.CharField(verbose_name='选项子标题', max_length=255)
+    sub_title_en = models.CharField(verbose_name='选项子标题(英文)', max_length=255)
     created_at = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     update_at = models.DateTimeField(verbose_name='更新时间', auto_now_add=True)
 
     class Meta:
         ordering = ['-id']
         verbose_name = verbose_name_plural = "选项表"
-
-
-@reversion.register()
-class Periods(models.Model):
-    periods = models.IntegerField(verbose_name='期数，递增，不中断')
-    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    start_value = models.DecimalField(verbose_name='当期开盘指数', max_digits=10, decimal_places=2, null=True)
-    lottery_value = models.DecimalField(verbose_name='当期开奖指数', max_digits=10, decimal_places=2, null=True)
-    lottery_time = models.DateTimeField(verbose_name='当前开奖时间', auto_now_add=True)
-    start_time = models.DateTimeField(verbose_name='开始下注时间', auto_now_add=True)
-    rotary_header_time = models.DateTimeField(verbose_name='封盘时间', auto_now_add=True)
-    is_seal = models.BooleanField(verbose_name="是否封盘", default=False)
-    up_and_down = models.CharField(verbose_name='涨跌正确选项', max_length=20, default='')
-    size = models.CharField(verbose_name='大小正确选项', max_length=20, default='')
-    points = models.CharField(verbose_name='点数正确选项', max_length=20, default='')
-    pair = models.CharField(verbose_name='对子正确选项', max_length=20, default='')
-    created_at = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
-    update_at = models.DateTimeField(verbose_name='更新时间', auto_now_add=True)
-
-    class Meta:
-        ordering = ['-id']
-        verbose_name = verbose_name_plural = "期数，开奖后生成下一期记录"
 
 
 @reversion.register()
@@ -193,7 +166,7 @@ class Record(models.Model):
         (ERROR, "异常")
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='guess_user')
-    club = models.ForeignKey(Club, on_delete=models.DO_NOTHING)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
     periods = models.ForeignKey(Periods, on_delete=models.CASCADE)
     options = models.ForeignKey(Options, on_delete=models.CASCADE)
     play = models.ForeignKey(Play, on_delete=models.CASCADE)
