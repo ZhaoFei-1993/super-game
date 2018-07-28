08e13b43731c50d27063919fc26867f7f5631d19
lvwenqi <769972594@qq.com>
2018-07-28 22:36:36 +0800

    1

--- guess/app/v1/views.py
+++ guess/app/v1/views.py
@@ -286,13 +286,9 @@ class BetView(ListCreateAPIView):
         # 单场比赛最大下注
         bet_sum = Record.objects.filter(user_id=user.id, club_id=club_id, periods_id=periods_id).aggregate(
             Sum('bets'))
+
         bet_sum = bet_sum['bets__sum'] if bet_sum['bets__sum'] else 0
-
-        print('--------------------------------------')
-        print('bet_sum=========', bet_sum)
-        print('--------------------------------------')
-
-        bet_sum = float(bet_sum['bets__sum']) + float(coins)
+        bet_sum = float(bet_sum) + float(coins)
 
         if coin_id == Coin.HAND:
             if bet_sum['bets__sum'] is not None and bet_sum['bets__sum'] >= 5000000:
