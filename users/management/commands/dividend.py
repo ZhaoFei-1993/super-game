0fe68516b3a011d6c92541be0e8c284a9f8c3d5c
zhijiefong <15812913276@139.com>
2018-07-28 20:50:06 +0800

    分红脚本

--- users/management/commands/dividend.py
+++ users/management/commands/dividend.py
@@ -2,6 +2,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from users.models import UserCoinLock, Dividend
 from quiz.models import Record, Quiz
+from guess.models import Record as Record_Guess
 from chat.models import Club
 from django.db.models import Q, Sum
 from utils.functions import normalize_fraction, get_sql
@@ -20,63 +21,112 @@ class Command(BaseCommand):
         total_gsg = 1000000000
         now_date = datetime.now().date()
         count_date = now_date - timedelta(1)
-        coin_locks = UserCoinLock.objects.filter(is_free=0, is_divided=0, created_at__date__lt=count_date)
-        sql = "select a.id from users_user a"
-        sql += " inner join (select ip_address, count(username) as count from users_user group by ip_address) b on a.ip_address=b.ip_address"
-        sql += " where b.count <=3 "
-        sql += " and is_robot=0 and is_block=0"
-        dt_all = list(get_sql(sql))
+        total_date = count_date.strftime('%Y-%m-%d')
+        coin_locks = UserCoinLock.objects.filter(is_free=0, is_divided=0, created_at__date__lt=count_date, end_time__gt=datetime.now())
+        print(total_date)
 
-        sql = "select distinct(user_id) from users_userrecharge"
-        dt_recharge = list(get_sql(sql))
+        #总下注额
+        sql = "select  club_id, sum(bet) from "
+        sql += " ("
+        sql += " select roomquiz_id as club_id,  sum(bet) as bet from quiz_record a "
+        sql += " join("
+        sql += " select  quiz_record.id from quiz_record "
+        sql += " where source <> '1'"
+        # sql += "-- and earn_coin > 0"
+        sql += " and  date_format(open_prize_time ,  '%Y-%m-%d') ='" + total_date +"'"
+        sql += " )  b  on b.id=a.id"
+        sql += " join ("
+        sql += " select quiz_id  from quiz_record c "
+        sql += " join (select  id from quiz_quiz where status='5') d on c.quiz_id = d.id"
+        sql += " group by quiz_id) e on e.quiz_id= a.quiz_id"
+        sql += " join( select a.id from users_user a"
+        sql += "         inner join (select ip_address, count(username) as count from users_user group by ip_address) b on a.ip_address=b.ip_address"
+        sql += "        where b.count <=3 "
+        sql += "        and is_robot=0 and is_block=0"
+        sql += "        union "
+        sql += "         select a.user_id as id from users_userrecharge a "
+        sql += "         join users_user b on a.user_id=b.id"
+        sql += "         where b.is_robot=0) f on f.id = a.user_id"
+        sql += " group by roomquiz_id"
+        sql += " union "
+        sql += " select   club_id, sum(bets) as bet  from guess_record a "
+        sql += " join( select a.id from users_user a"
+        sql += "         inner join (select ip_address, count(username) as count from users_user group by ip_address) b on a.ip_address=b.ip_address"
+        sql += "        where b.count <=3 "
+        sql += "        and is_robot=0 and is_block=0"
+        sql += "        union "
+        sql += "         select a.user_id as id from users_userrecharge a "
+        sql += "         join users_user b on a.user_id=b.id"
+        sql += "         where b.is_robot=0) c on c.id = a.user_id"
+        sql += " where status = '1'"
+        sql += " and source <> '4'"
+        # sql += "-- and earn_coin > 0"
+        sql += " and date_format(update_at, '%Y-%m-%d') = '" + total_date +"'"
+        sql += " group by club_id) v"
+        sql += " group by club_id"
+        dt_all = self.list2dict(list(get_sql(sql)))
 
-        dt_all = list(set(dt_all + dt_recharge))
-        dd = []
-        for x in dt_all:
-            dd.append((x[0]))
-        # users = '(' + ','.join(dd) + ')'
+        #总发放额
+        sql = "select  club_id, sum(earn_coin) from "
+        sql += " ("
+        sql += " select roomquiz_id as club_id,  sum(earn_coin) as earn_coin from quiz_record a "
+        sql += " join("
+        sql += " select  quiz_record.id from quiz_record "
+        sql += " where source <> '1'"
+        sql += " and earn_coin > 0"
+        sql += " and  date_format(open_prize_time ,  '%Y-%m-%d') ='" + total_date +"'"
+        sql += " )  b  on b.id=a.id"
+        sql += " join ("
+        sql += " select quiz_id  from quiz_record c "
+        sql += " join (select  id from quiz_quiz where status='5') d on c.quiz_id = d.id"
+        sql += " group by quiz_id) e on e.quiz_id= a.quiz_id"
+        sql += " join( select a.id from users_user a"
+        sql += "         inner join (select ip_address, count(username) as count from users_user group by ip_address) b on a.ip_address=b.ip_address"
+        sql += "        where b.count <=3 "
+        sql += "        and is_robot=0 and is_block=0"
+        sql += "        union "
+        sql += "         select a.user_id as id from users_userrecharge a "
+        sql += "         join users_user b on a.user_id=b.id"
+        sql += "         where b.is_robot=0) f on f.id = a.user_id"
+        sql += " group by roomquiz_id"
+        sql += " union "
+        sql += " select   club_id, sum(earn_coin) as earn_coin  from guess_record a "
+        sql += " join( select a.id from users_user a"
+        sql += "         inner join (select ip_address, count(username) as count from users_user group by ip_address) b on a.ip_address=b.ip_address"
+        sql += "        where b.count <=3 "
+        sql += "        and is_robot=0 and is_block=0"
+        sql += "        union "
+        sql += "         select a.user_id as id from users_userrecharge a "
+        sql += "         join users_user b on a.user_id=b.id"
+        sql += "         where b.is_robot=0) c on c.id = a.user_id"
+        sql += " where status = '1'"
+        sql += " and source <> '4'"
+        sql += " and earn_coin > 0"
+        sql += " and date_format(update_at, '%Y-%m-%d') = '" + total_date +"'"
+        sql += " group by club_id) v"
+        sql += " group by club_id"
+        dt_earn = self.list2dict(list(get_sql(sql)))
+        if dt_all:
+            temp_dict={}
+            for x in dt_all:
+                if x not in dt_earn:
+                    temp_dict[x] = dt_all[x]
+                else:
+                    temp_dict[x] = normalize_fraction(dt_all[x]-dt_earn[x],18)
 
-        # quizs= Quiz.objects.filter(status=Quiz.BONUS_DISTRIBUTION)
-
-        bets = Record.objects.filter(~Q(source=Record.CONSOLE), quiz__status=Quiz.BONUS_DISTRIBUTION, open_prize_time__date=count_date, user_id__in=dd).values('roomquiz_id').annotate(Sum('bet')).order_by('roomquiz_id')
-        sent_coin = Record.objects.filter(~Q(source=Record.CONSOLE), quiz__status=Quiz.BONUS_DISTRIBUTION, open_prize_time__date=count_date, user_id__in=dd, earn_coin__gt= 0).values('roomquiz_id').annotate(Sum('earn_coin')).order_by('roomquiz_id')
-        temp_list=[]
-        if bets.exists():
-            if not sent_coin.exists():
-                for a in bets:
-                    temp_list.append({
-                        'roomquiz_id': a['roomquiz_id'],
-                        'profit': normalize_fraction(a['bet__sum'], 18)
-                    })
-            else:
-                rooms = []
-                earns = {}
-                for b in sent_coin:
-                    rooms.append(b['roomquiz_id'])
-                    earns[str(b['roomquiz_id'])] = b['earn_coin__sum']
-                for a in bets:
-                    if a['roomquiz_id'] not in rooms:
-                        temp_list.append({
-                            'roomquiz_id': a['roomquiz_id'],
-                            'profit': normalize_fraction(a['bet__sum'], 18)
-                        })
-                    else:
-                        temp_list.append({
-                            'roomquiz_id': a['roomquiz_id'],
-                            'profit': normalize_fraction(a['bet__sum'] - earns[str(a['roomquiz_id'])], 18)})
-
+            clubs = Club.objects.all()
             coins = []
-            if temp_list:
-                for x in temp_list:
-                    try:
-                        club = Club.objects.get(id=x['roomquiz_id'])
-                    except Exception:
-                        raise
-                    if club.coin.is_criterion != 1:
-                        coins.append(
-                            {'coin_id':club.coin_id,
-                             'profit':x['profit']
-                         })
+            for x in clubs:
+                for y in temp_dict.keys():
+                    if x.id == int(y):
+                        if x.coin.is_criterion!=1:
+                            coins.append(
+                                {
+                                    'coin_id':x.coin_id,
+                                    'profit':temp_dict[y]
+                                }
+                        )
+            print('各种主要币种营收情况：', coins)
             if coin_locks.exists():
                 for x in coin_locks:
                     for y in coins:
@@ -91,3 +141,11 @@ class Command(BaseCommand):
                 raise CommandError('当前无锁定记录')
         else:
             self.stdout.write(self.style.SUCCESS('今日无开奖无分红'))
+
+
+    def list2dict(self, result):
+        temp={}
+        if result:
+            for x in result:
+                temp[str(x[0])]=x[1]
+        return temp
