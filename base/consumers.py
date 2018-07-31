0f0d276efa928b7a336f4b7d86ffce1179a4e575
lvwenqi <769972594@qq.com>
2018-08-01 01:06:56 +0800

    股指封盘推送

--- base/consumers.py
+++ base/consumers.py
@@ -122,6 +122,11 @@ class QuizConsumer(AsyncJsonWebsocketConsumer):
                 "type": "basketball_time.message",
                 "quiz_id": content.get("quiz_id"),
             })
+        elif command == 'stock_seal':
+            await self.channel_layer.group_send(group_name, {
+                "type": "stock_time.message",
+                "period_id": content.get("period_id"),
+            })
 
         # elif command == 'send':
         #     await self.channel_layer.group_send(group_name, {
@@ -206,3 +211,7 @@ class QuizConsumer(AsyncJsonWebsocketConsumer):
             pass
         else:
             call_command('basketball_synctime', quiz_id)
+
+    async def stock_time_message(self, event):
+        period_id = event['period_id']
+        call_command('stock_synctime', period_id)
