758b36abc0b0c35b53279eef4b75893dd39c044a
Rex <rexlin0624@gmail.com>
2018-08-01 10:39:07 +0700

    fff

--- base/management/commands/rq_worker.py
+++ base/management/commands/rq_worker.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from rq import Connection, Worker
+from rq import Connection, Worker, use_connection
 from redis import Redis
+import local_settings
 
 
 class Command(BaseCommand):
     help = "RedisQueue队列命令"
 
     def handle(self, *args, **options):
+        config = local_settings.CHANNEL_LAYERS['default']['CONFIG']['hosts'][0]
+
         with Connection():
             qs = args[1:] or ['default']
 
-            redis_conn = Redis()
-            w = Worker(qs, connection=redis_conn)
+            redis_conn = Redis(config[0], config[1])
+            use_connection(redis_conn)
+            w = Worker(qs)
             w.work()
 
