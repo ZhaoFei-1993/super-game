4431477179ec18b51b6f504ba4069adfe8484d3a
lvwenqi <769972594@qq.com>
2018-08-03 17:41:19 +0800

    1

--- handle/management/commands/calculate_record_sum.py
+++ handle/management/commands/calculate_record_sum.py
@@ -54,7 +54,8 @@ class Command(BaseCommand):
                 if coin_name in record_dic.keys():
                     record_dic[coin_name] = record_dic[coin_name] + float(record_personal.bet)
                 else:
-                    record_dic[coin_name] = float(record_personal.bet)
+                    record_dic[coin_name] = normalize_fraction(record_personal.bet,
+                                                               int(record_personal.club.coin.coin_accuracy))
 
                 rmb_price = 'currency_corresponds_to_rmb_price_'+str(coin_name)
                 usd_price = 'currency_corresponds_to_usd_price_' + str(coin_name)
