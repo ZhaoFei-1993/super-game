1398d3a3b46b86b5960844d0a322efa101560b80
Rex <rexlin0624@gmail.com>
2018-08-07 15:24:23 +0700

    特殊用户锁定处理

--- users/backend/views.py
+++ users/backend/views.py
@@ -2111,7 +2111,9 @@ class CoinDividendProposalView(ListCreateAPIView):
 
         # 写入虚拟盈利数据表中
         club_profit_date = dateparser.parse(datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d 23:59:59.000000'))
+        print('club_profit_date = ', club_profit_date)
         club_profits = ClubProfitAbroad.objects.filter(created_at=club_profit_date)
+        print('club_profits = ', club_profits)
         for profit in club_profits:
             for club_id in map_club_id_amount:
                 if profit.roomquiz_id != club_id:
