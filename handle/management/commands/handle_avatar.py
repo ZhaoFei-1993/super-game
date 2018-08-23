17e754c517b46dee621fd0aa3717184c95a1e957
lvwenqi <769972594@qq@com>
2018-08-23 17:55:31 +0800

    处理队标

--- handle/management/commands/handle_avatar.py
+++ handle/management/commands/handle_avatar.py
@@ -12,21 +12,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         img_dir = BASE_DIR + '/uploads/images/spider/football/team_icon'
         os.chdir(img_dir)
+        host_avatar_now = 'https://api.gsg.one/uploads/images/spider/football/team_icon/pres_proj_2018_201806_TeamLogo_vs_fb_home_1.png'
+        guest_avatar_now = 'https://api.gsg.one/uploads/images/spider/football/team_icon/pres_proj_2018_201806_TeamLogo_vs_fb_visiting_1.png'
         for quiz in Quiz.objects.filter(category__parent_id=2):
-            try:
-                host_name = quiz.host_team
-                guest_name = quiz.guest_team
-                host_avatar = quiz.host_team_avatar.split('/')[-1]
-                guest_avatar = quiz.guest_team_avatar.split('/')[-1]
-                host_avatar_type = host_avatar.split('.')[1]
-                guest_avatar_type = guest_avatar.split('.')[1]
+            if quiz.host_team_avatar != host_avatar_now and quiz.guest_team_avatar != guest_avatar_now:
+                try:
+                    host_name = quiz.host_team
+                    guest_name = quiz.guest_team
+                    host_avatar = quiz.host_team_avatar.split('/')[-1]
+                    guest_avatar = quiz.guest_team_avatar.split('/')[-1]
+                    host_avatar_type = host_avatar.split('.')[1]
+                    guest_avatar_type = guest_avatar.split('.')[1]
 
-                print(host_avatar, '===', host_name + '.' + host_avatar_type)
-                print(guest_avatar, '===', guest_name + '.' + guest_avatar_type)
+                    print(host_avatar, '===', host_name + '.' + host_avatar_type)
+                    print(guest_avatar, '===', guest_name + '.' + guest_avatar_type)
 
-                if os.path.exists(host_name + '.' + host_avatar_type) is not True and os.path.exists(host_avatar) is True:
-                    shutil.copy(host_avatar, host_name + '.' + host_avatar_type)
-                if os.path.exists(guest_name + '.' + guest_avatar_type) is not True and os.path.exists(guest_avatar) is True:
-                    shutil.copy(guest_avatar, guest_name + '.' + guest_avatar_type)
-            except Exception as e:
-                pass
+                    if os.path.exists(host_name + '.' + host_avatar_type) is not True and os.path.exists(host_avatar) is True:
+                        shutil.copy(host_avatar, host_name + '.' + host_avatar_type)
+                    if os.path.exists(guest_name + '.' + guest_avatar_type) is not True and os.path.exists(guest_avatar) is True:
+                        shutil.copy(guest_avatar, guest_name + '.' + guest_avatar_type)
+                except Exception as e:
+                    pass
