4141e872eedc1bca34a230792ccce5438ec7bc77
Rex <rexlin0624@gmail.com>
2018-08-25 08:54:27 +0700

    调整机器人下注

--- users/management/commands/robot_bet.py
+++ users/management/commands/robot_bet.py
@@ -373,7 +373,6 @@ class Command(BaseCommand):
         :return:
         """
         users = User.objects.filter(is_robot=True)
-        print("users=====================================", users)
         secure_random = random.SystemRandom()
         return secure_random.choice(users)
 
