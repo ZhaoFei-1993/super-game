5e754e384f18e68fe38c8199827845baa019228e
lvwenqi <769972594@qq@com>
2018-08-25 11:23:19 +0800

    1

--- spider/management/commands/football_match.py
+++ spider/management/commands/football_match.py
@@ -25,7 +25,7 @@ def get_data(url):
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             dt = response.text.encode("utf-8").decode('unicode_escape')
-            result = json.loads(dt[12:-2])
+            result = json.loads(dt[8:-2])
             return result
     except requests.ConnectionError as e:
         print('match Error ', e.args)
