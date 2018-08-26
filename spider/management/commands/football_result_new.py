eaca67982c12059d57364bb93153243483abaa95
Rex <rexlin0624@gmail.com>
2018-08-26 16:40:17 +0700

    s

--- spider/management/commands/football_result_new.py
+++ spider/management/commands/football_result_new.py
@@ -735,7 +735,7 @@ class Command(BaseCommand):
             print(list(quizs))
             for quiz in list(quizs)[:10]:
                 print('quiz.match_flag = ', quiz.match_flag)
-                if int(quiz.match_flag) in [110208, 110322, 110207]:
+                if int(quiz.match_flag) in [110208, 110322, 110207, 110200]:
                     print('玩法Rule数据不全，跳过')
                     continue
                 if int(Quiz.objects.filter(match_flag=quiz.match_flag).first().status) != Quiz.BONUS_DISTRIBUTION:
