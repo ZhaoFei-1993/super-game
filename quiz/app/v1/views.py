3ced5437c991e82e47ff7c2ed20fb9a00c3a8d64
q84256948 <1418229458@qq.com>
2018-09-03 08:35:55 +0700

    修改

--- quiz/app/v1/views.py
+++ quiz/app/v1/views.py
@@ -1301,12 +1301,14 @@ class ChangeRemainder(ListAPIView):
         yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
         EXCHANGE_QUALIFICATION = "exchange_qualification_" + user_id + '_' + str(day)  # key
         number = get_cache(EXCHANGE_QUALIFICATION)
+        print("number=====================================", number)
         if number == None or number == '':
             everydayinjection = EveryDayInjectionValue.objects.filter(user_id=int(user_id), injection_time=yesterday)
             if len(everydayinjection) <= 0:
                 raise ParamErrorException(error_code.API_70208_NO_REDEMPTION)  # 有没有兑换资格
             else:
                 number = everydayinjection[0].order
+        print("number====================================", number)
         if int(number) < 1000:
             raise ParamErrorException(error_code.API_70208_NO_REDEMPTION)  # 有没有兑换资格
 
