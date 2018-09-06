c2e703e797c6707abed1a16a57ed0bfde1979814
lvwenqi <769972594@qq@com>
2018-09-06 11:07:32 +0800

    1

---
+++ handle/management/commands/handle_stock_result.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from spider.management.commands.stock_result_new import ergodic_record
+from guess.models import Periods
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        dt = {'num': '2704.34', 'status': 'up'}
+        period = Periods.objects.get(id=234)
+        ergodic_record(period, dt)
+
+        dt = {'num': '8402.51', 'status': 'up'}
+        period = Periods.objects.get(id=235)
+        ergodic_record(period, dt)
+
+        dt = {'num': '27243.85', 'status': 'up'}
+        period = Periods.objects.get(id=236)
+        ergodic_record(period, dt)
