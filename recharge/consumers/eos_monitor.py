806bdbfaf74ed544dddb89e3f1970b4550191ab7
Rex <rexlin0624@gmail.com>
2018-09-10 20:49:53 +0700

    增加EOS接口

--- recharge/consumers/eos_monitor.py
+++ recharge/consumers/eos_monitor.py
@@ -1,5 +1,5 @@
 import time
-from users.models import UserRecharge, UserCoin, Coin
+from users.models import UserRecharge, User, Coin
 from base.eth import Wallet
 from decimal import Decimal
 from local_settings import EOS_WALLET_API_URL
@@ -20,7 +20,7 @@ def electro_optical_system_monitor(block_num):
     json_obj = wallet.get(url=EOS_WALLET_API_URL + 'v1/eos/block/transactions/' + str(block_num))
     block = json_obj['data']
 
-    print('block = ', block)
+    # 格式化时间
     block_time = block['time']
     t_year = int(block_time[:4])
     t_month = int(block_time[5:7])
@@ -30,28 +30,26 @@ def electro_optical_system_monitor(block_num):
     t_second = int(block_time[17:19])
     t_msecond = int(block_time[20:23])
     block_time = datetime(t_year, t_month, t_day, t_hour, t_minute, t_second, t_msecond)
-    print(convert_localtime(block_time))
-    raise 404
 
-    to_address = []
-    address_tx = {}
+    to_memos = []
+    memo_tx = {}
     for index, item in enumerate(block['transactions']):
         outputs = item['out']
         for output in outputs:
-            addresses = output['addresses']
-            to_address += addresses
-            for address in addresses:
-                if address not in address_tx:
-                    address_tx[address] = []
+            memos = 100000 if output['memo'] == 'memo' else output['memo']
+            to_memos += memos
+            for memo in memos:
+                if memo not in memo_tx:
+                    memo_tx[memo] = []
 
-                address_tx[address].append({
+                memo_tx[memo].append({
                     'txid': item['txid'],
                     'value': output['value'],
                     'memo': output['memo'],
                 })
 
-    in_address = UserCoin.objects.filter(address__in=to_address).values('address', 'user_id')
-    if len(in_address) == 0:
+    in_memo = User.objects.filter(eos_code__in=to_memos).values('id', 'memo')
+    if len(in_memo) == 0:
         return True
 
     # 所有充值记录
@@ -62,24 +60,26 @@ def electro_optical_system_monitor(block_num):
 
     # 把该地址对应的交易信息拿出来
     recharge_number = 0
-    for user_coin in in_address:
-        address_recharges = address_tx[user_coin.address]
-        for recharge in address_recharges:
+    for user in in_memo:
+        memo_recharges = memo_tx[user.eso_code]
+        for recharge in memo_recharges:
             txid = recharge['txid']
             if txid in txids:
                 continue
 
             recharge_obj = UserRecharge()
-            recharge_obj.address = user_coin.address
+            recharge_obj.address = user.eso_code
             recharge_obj.coin_id = Coin.EOS
             recharge_obj.txid = txid
-            recharge_obj.user_id = user_coin.user_id
+            recharge_obj.user_id = user.id
             recharge_obj.amount = Decimal(recharge['value'])
             recharge_obj.confirmations = 0
             recharge_obj.trade_at = convert_localtime(block_time)
-            recharge_obj.save()
+            # recharge_obj.save()
 
-            print('获取1条EOS充值记录，TX = ', txid, ' 充值地址 = ', user_coin.address, ' 充值金额 = ', recharge['value'])
+            # user coin增加对应值
+
+            print('获取1条EOS充值记录，TX = ', txid, ' 充值码 = ', str(user.eso_code), ' 充值金额 = ', recharge['value'])
 
             recharge_number += 1
 
