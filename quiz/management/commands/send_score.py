3ee8dafbe069af1fb7f2acddfd88c93144cec25a
Rex <rexlin0624@gmail.com>
2018-09-14 14:06:58 +0700

    test send score

--- quiz/management/commands/send_score.py
+++ quiz/management/commands/send_score.py
@@ -4,16 +4,16 @@ from django.core.management.base import BaseCommand
 from rq import Queue
 from redis import Redis
 from quiz.consumers import quiz_send_score
+from local_settings import CHANNEL_LAYERS
 
 
 class Command(BaseCommand):
     help = "推送竞猜比分"
 
     def handle(self, *args, **options):
-        quiz_id = 1660
-        host = 1
-        guest = 2
+        config = CHANNEL_LAYERS['default']['CONFIG']['hosts'][0]
+        host, port = config
 
-        redis_conn = Redis()
+        redis_conn = Redis(host, port)
         q = Queue(connection=redis_conn)
-        q.enqueue(quiz_send_score, quiz_id, host, guest)
+        q.enqueue(quiz_send_score, 1660, 1, 2)
