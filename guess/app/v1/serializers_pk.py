e7270861668fc15cfdca2bc85d3152d56e2e318f
lvwenqi <769972594@qq@com>
2018-09-18 15:52:49 +0700

    股指pk接口

---
+++ guess/app/v1/serializers_pk.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+from rest_framework import serializers
+from guess.models import StockPk, Issues, PlayStockPk, OptionStockPk, RecordStockPk, BetLimit
+
+
+class StockPkResultListSerialize(serializers.ModelSerializer):
+    """
+    股指pk开奖记录
+    """
+    open_time = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Issues
+        fields = ('id', 'stock_pk_id', 'issue', 'open', 'left_stock_index',
+                  'right_stock_index', 'size_pk_result', 'open_time')
+
+    @staticmethod
+    def get_open_time(obj):
+        return obj.open.strftime('%Y-%m-%d %H:%M:%S')
