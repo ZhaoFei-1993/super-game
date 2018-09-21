66fe857ef52ee83f0bdb25efb133234802fc119d
lvwenqi <769972594@qq@com>
2018-09-21 16:32:28 +0700

    股票推送测试

--- spider/management/commands/test.py
+++ spider/management/commands/test.py
@@ -3,6 +3,7 @@
 from django.core.management.base import BaseCommand
 from guess.models import Issues, Periods
 from guess.consumers import guess_pk_detail, guess_pk_result_list
+from guess.consumers import guess_graph
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
@@ -14,8 +15,10 @@ class Command(BaseCommand):
     help = "发送消息"
 
     def handle(self, *args, **options):
-        guess_pk_detail(100)
-        guess_pk_result_list(100)
+        index_list = ['1234.71', '1298.71', '1245.70', '1567.70', '1111.70']
+        guess_graph(178, index_list)
+        guess_graph(179, list(reversed(index_list)))
+        print('成功')
 
 
 
