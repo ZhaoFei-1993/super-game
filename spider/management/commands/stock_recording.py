7226150f80719775ebcd19a73fd662486948e0ac
lvwenqi <769972594@qq@com>
2018-09-21 12:02:08 +0700

    股指PK开奖

--- spider/management/commands/stock_recording.py
+++ spider/management/commands/stock_recording.py
@@ -10,22 +10,24 @@ class Command(BaseCommand):
     help = "处理投注表"
 
     def handle(self, *args, **options):
-        record_stock_pk = GuessPKRecording()
-        option_obj_dic = {}
-        for option in OptionStockPk.objects.all():
-            option_obj_dic.update({
-                option.id: {
-                    'title': option.title,
-                }
-            })
-        i = 0
-        for issues in Issues.objects.filter(result_confirm__gte=3, is_open=False).order_by('id')[:10]:
-            i += 1
-            print('正在处理第 ', i, ' 期,issues_id=', issues.id, '   ', datetime.datetime.now())
-            for record_pk in RecordStockPk.objects.filter(issue_id=issues.id, status=str(RecordStockPk.AWAIT)):
-                record_stock_pk.pk_size(record_pk, option_obj_dic, issues)
-                record_stock_pk.insert_info()
-            issues.is_open = True
-            issues.save()
-        if i == 0:
+        issues = Issues.objects.filter(result_confirm__gte=3, is_open=False).order_by('id')[:10]
+        if len(issues) > 0:
+            record_stock_pk = GuessPKRecording()
+            option_obj_dic = {}
+            for option in OptionStockPk.objects.all():
+                option_obj_dic.update({
+                    option.id: {
+                        'title': option.title,
+                    }
+                })
+            i = 0
+            for issue in issues:
+                i += 1
+                print('正在处理第 ', i, ' 期,issues_id=', issue.id, '   ', datetime.datetime.now())
+                for record_pk in RecordStockPk.objects.filter(issue_id=issue.id, status=str(RecordStockPk.AWAIT)):
+                    record_stock_pk.pk_size(record_pk, option_obj_dic, issue)
+                    record_stock_pk.insert_info()
+                issue.is_open = True
+                issue.save()
+        else:
             print('当前无需要处理的数据')
