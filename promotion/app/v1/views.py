554b9b1cee24bce577088654b0421cec4ccb11eb
yu <1418229458@qq.com>
2018-10-18 10:58:28 +0800

    soc活动

--- promotion/app/v1/views.py
+++ promotion/app/v1/views.py
@@ -776,13 +776,11 @@ class ClubDetailView(ListAPIView):
                 "pecific_dates": pecific_dates,
                 "pecific_date": pecific_date,
             })
-        print("bet_water==============================", bet_water)
         dividend_water = Decimal(bet_water)*Decimal(0.005)
-        print("dividend_water=============================", dividend_water)
+        print("normalize_fraction(bet_water, coin_accuracy)=======================", normalize_fraction(bet_water, coin_accuracy))
         bet_water = str(normalize_fraction(bet_water, coin_accuracy)) + " " + coin_name
-        print("bet_water=============================", bet_water)
+        print("normalize_fraction(dividend_water, coin_accuracy) ==================================", normalize_fraction(dividend_water, coin_accuracy))
         dividend_water = str(normalize_fraction(dividend_water, coin_accuracy)) + " " + coin_name
-        print("dividend_water=====================================", dividend_water)
 
         return self.response({'code': 0,
                               "invite_number": len(user_list),
