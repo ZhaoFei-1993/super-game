78af02473a29ed1d9e2c8dc0b826106defc45314
lvwenqi <769972594@qq@com>
2018-10-23 17:39:58 +0700

    fix

--- guess/app/v1/views.py
+++ guess/app/v1/views.py
@@ -51,6 +51,7 @@ class StockList(ListAPIView):
             self.last_periods_list.append(period)
             if period.periods - 1 != 0:
                 pre_period.append({period.stock_id: period.periods - 1})
+        print('pre_period =========== ', pre_period)
         # 取上期
         for pre_period in Periods.objects.filter(
                 Q(Q(stock_id=list(pre_period[0].keys())[0]) | Q(periods=list(pre_period[0].values())[0])) | Q(
