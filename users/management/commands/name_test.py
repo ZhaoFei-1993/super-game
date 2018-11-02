f7946ef7f6150f3273ad849aafe213431bc827d1
lvwenqi <769972594@qq@com>
2018-11-02 22:26:17 +0700

    删除冗余数据

--- users/management/commands/name_test.py
+++ users/management/commands/name_test.py
@@ -2,16 +2,27 @@
 
 from django.core.management.base import BaseCommand
 from base.app import BaseView
-from users.models import UserMessage, CoinDetail
+from users.models import UserMessage, CoinDetail, User
+from django.db import connection
 
 
 class Command(BaseCommand, BaseView):
     help = "删除"
 
     def handle(self, *args, **options):
+        print('开始搜索robot id')
+        robot_list = User.objects.filter(is_robot=1).values_list('id', flat=True)
+        robot_tuple = tuple(robot_list)
+        print('开始搜索robot id结束')
+
         print("信息表删除开始")
-        UserMessage.objects.filter(user__is_robot=1).delete()
+        sql = 'DELETE FROM users_usermessage WHERE user_id IN ' + str(robot_tuple) + ';'
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
         print("信息表删除完成")
+
         print("资金记录表删除开始")
-        CoinDetail.objects.filter(user__is_robot=1).delete()
+        sql = 'DELETE FROM users_coindetail WHERE user_id IN ' + str(robot_tuple) + ';'
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
         print("资金记录表删除完成")
