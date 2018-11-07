a42e7b7f636916055d39c84008452676d7ee12ad
q84256948 <1418229458@qq.com>
2018-11-07 14:51:16 +0700

    转账

--- promotion/management/commands/monthly_settlement.py
+++ promotion/management/commands/monthly_settlement.py
@@ -37,6 +37,8 @@ class Command(BaseCommand, BaseView):
         for i in userpresentation_list:
             income = i[2]
             print("income=============================", income)
+            print("user_id=============================", i[3])
+            print("coin_id=============================", I[5])
             proportion = reward_gradient_all(i[4], i[2])
             income_dividend = proportion*opposite_number(i[2])
             presentation_month = PresentationMonth()
