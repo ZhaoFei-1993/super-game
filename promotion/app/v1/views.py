aa12c167cb0955542da46892d840bb4fe01dbc86
q84256948 <1418229458@qq.com>
2018-12-03 11:43:23 +0700

    联合做庄，认购接口修改

--- promotion/app/v1/views.py
+++ promotion/app/v1/views.py
@@ -298,24 +298,10 @@ class ClubRuleView(ListAPIView):
         return
 
     def list(self, request, *args, **kwargs):
-        sql = "select name from quiz_category"
-        # sql += " where id in (1, 2)"
-        # quiz_rule = get_sql(sql)
         data = []
-        # for s in quiz_rule:
-        #     if s[0] == "篮球":
-        #         number = 2
-        #         a = 2
-        #     else:
-        #         number = 3
-        #         a = 1
-        #     data.append({
-        #         "name": s[0],
-        #         "number": number,
-        #         "a": a
-        #     })
 
         sql = "select title from chat_clubrule"
+        sql += " where is_deleted = 0"
         club_rule_info = get_sql(sql)
         for i in club_rule_info:
             if i[0] == "猜股指":
