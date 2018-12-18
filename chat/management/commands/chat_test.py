3d83a3396e127ed37514b6c7957810e76e57f471
q84256948 <1418229458@qq.com>
2018-12-18 15:40:29 +0700

    联合做庄，认购接口修改

---
+++ chat/management/commands/chat_test.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+
+from django.core.management.base import BaseCommand
+from base.app import BaseView
+from chat.models import ClubIncome, Club, ClubRule
+from utils.functions import get_sql
+from promotion.models import PromotionRecord
+from users.models import CoinDetail
+from decimal import Decimal
+from django.conf import settings
+
+
+class Command(BaseCommand, BaseView):
+    help = "联合做庄结算方法测试"
+
+    def handle(self, *args, **options):
+        user_list = str(settings.TEST_USER_IDS)
+        club_list = Club.objects.get_all()
+        for i in club_list:
+            club_info = i
+            club_id = i.id
+            if club_id == 1:
+                continue
+            club_rule = ClubRule.objects.all()
+            info = {}
+            for s in club_rule:
+                type = int(s.id)
+                if int(type) == 7:
+                    type = 2
+                elif int(type) == 1:
+                    type = 1
+                elif int(type) == 3:
+                    type = 4
+                elif int(type) == 2:
+                    type = 3
+                elif int(type) == 4:
+                    type = 7
+                    continue
+                elif int(type) == 5:
+                    type = 6
+                    continue
+                elif int(type) == 6:
+                    type = 5
+                sql_list = "date_format( p.created_at, '%Y-%m-%d' ) as years, "
+                if type == 1:  # 足球
+                    sql_list += "r.quiz_id as key_id, "
+                elif type == 2:  # 篮球
+                    sql_list += "r.quiz_id as key_id, "
+                elif type == 3:  # 六合彩
+                    sql_list += "r.issue as key_id, "
+                elif type == 4:  # 股票
+                    sql_list += "r.periods_id as key_id, "
+                elif type == 5:  # 股票PK
+                    sql_list += "r.issue_id as key_id, "
+                sql_list += "sum(IF(p.earn_coin > 0, 0 - (p.earn_coin - p.bets), ABS(p.earn_coin))) as profit, "
+                sql_list += "p.source"
+                sql = "select " + sql_list + " from promotion_promotionrecord p"
+                if type == 1:  # 足球
+                    sql += " inner join quiz_record r on p.record_id=r.id"
+                elif type == 2:  # 篮球
+                    sql += " inner join quiz_record r on p.record_id=r.id"
+                elif type == 3:  # 六合彩
+                    sql += " inner join marksix_sixrecord r on p.record_id=r.id"
+                elif type == 4:  # 股票
+                    sql += " inner join guess_record r on p.record_id=r.id"
+                elif type == 5:  # 股票PK
+                    sql += " inner join guess_recordstockpk r on p.record_id=r.id"
+                sql += " where p.club_id = '" + str(club_id) + "'"
+                sql += " and p.source = '" + str(type) + "'"
+                sql += " and p.user_id not in " + user_list
+                sql += " group by years, key_id"
+                # sql += " order by years desc"
+                print("sql==========", sql)
+                list_info = get_sql(sql)
+                print("list_info================", list_info)
+                for a in list_info:
+                    print("1111111111111111")
+                    key_name = ""
+                    if a[3] == 1:
+                        key_name = "足球"
+                    elif a[3] == 2:
+                        key_name = "篮球"
+                    elif a[3] == 3:
+                        key_name = "六合彩"
+                    elif a[3] == 4:
+                        key_name = "股票"
+                    elif a[3] == 5:
+                        key_name = "股票PK"
+                    if key_name not in info:
+                        info[key_name] = {}
+                        if a[0] not in info[key_name]:
+                            number = a[2]
+                            if number > 0:
+                                number = number*Decimal(str(0.95))
+                            info[key_name][a[0]] = number
+                        else:
+                            number = a[2]
+                            if number > 0:
+                                number = number * Decimal(str(0.95))
+                            info[key_name][a[0]] += number
+                    else:
+                        if a[0] not in info[key_name]:
+                            number = a[2]
+                            if number > 0:
+                                number = number * Decimal(str(0.95))
+                            info[key_name][a[0]] = number
+                        else:
+                            number = a[2]
+                            if number > 0:
+                                number = number * Decimal(str(0.95))
+                            info[key_name][a[0]] += number
+            print("info =================", info)
